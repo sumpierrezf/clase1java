685a019f1bdbbcebfd597974066284a41ed4d175
sumpierrezf <viqui09umpierrez@gmail.com>
2024-05-27 18:19:11 -0300

    Conceptos basicos de Java

---
+++ src/Mascotas.java
@@ -0,0 +1,87 @@
+public class Mascotas {
+    public static void main(String[] args) {
+        //Perro
+        String mascota1 = "Primer mascota: ";
+        String tipoPerro = "perro";
+        String nombrePerro = "Manchitas";
+        int edadPerro = 2;
+        double cuantoComePerro = 1.5;  // en kilos
+        String sonidoPerro = "guau guau";
+
+        // Mostrar la info del perro
+        System.out.println("Presentando a la " + mascota1);
+        System.out.println("¿Qué es? Un " + tipoPerro);
+        System.out.println("¿Cómo lo llamo? " + nombrePerro);
+        System.out.println("¿Cuántos años tiene? " + edadPerro + " años");
+        System.out.println("¿Cuánto come? " + cuantoComePerro + " kilos.");
+        System.out.println("¿Cómo hace el ladrido? " + sonidoPerro);
+        System.out.println();
+
+        //Pez
+        String mascota2 = "Segunda mascota: ";
+        String tipoPez = "pez";
+        String nombrePez = "Nemo";
+        int edadPez = 1;
+        double cuantoComePez = 0.14;
+        String sonidoPez = "Blu, blu, blu";
+
+        //Mostrar info del pez
+        System.out.println("Presentando a la " + mascota2);
+        System.out.println("¿Qué es? Un " + tipoPez);
+        System.out.println("¿Cómo lo llamo? " + nombrePez);
+        System.out.println("¿Cuántos años tiene? " + edadPez + " año");
+        System.out.println("¿Cuánto come? " + cuantoComePez + " kilo.");
+        System.out.println("¿Qué sonido hacen sus burbujas de aire en el agua? " + sonidoPez);
+        System.out.println();
+
+        //Gato
+        String mascota3 = "Tercer mascota: ";
+        String tipoGato = "gato";
+        String nombreGato = "Silvestre";
+        int edadGato = 3;
+        double cuantoComeGato = 0.5;
+        String sonidoGato = "Miau, miau";
+
+        //Mostrar info del gato
+        System.out.println("Presentando a la " + mascota3);
+        System.out.println("¿Qué es? Un " + tipoGato);
+        System.out.println("¿Cómo lo llamo? " + nombreGato);
+        System.out.println("¿Cuántos años tiene? " + edadGato + " años");
+        System.out.println("¿Cuánto come? " + cuantoComeGato + " kilo.");
+        System.out.println("¿Cómo suena su maullido? " + sonidoGato);
+        System.out.println();
+
+        //Tortuga
+        String mascota4 = "Cuarta mascota: ";
+        String tipoTortuga = "tortuga";
+        String nombreTortuga = "Manuelita";
+        int edadTortuga = 12;
+        double cuantoComeTortuga = 0.3;
+        String dondeVivia = "En Pehuajó";
+
+        //Mostar info de la tortuga
+        System.out.println("Presentando a la " + mascota4);
+        System.out.println("¿Qué es? Un " + tipoTortuga);
+        System.out.println("¿Cómo lo llamo? " + nombreTortuga);
+        System.out.println("¿Cuántos años tiene? " + edadTortuga + " años");
+        System.out.println("¿Cuánto come? " + cuantoComeTortuga + " kilo.");
+        System.out.println("Según la canción, ¿donde vivía? " + dondeVivia);
+        System.out.println();
+
+        //Canario
+        String mascota5 = "Quinta mascota: ";
+        String tipoCanario = "canario";
+        String nombreCanario = "Gardel";
+        int edadCanario = 1;
+        double cuantoComeCanario = 0.35;
+        String sonidoCanario = "Pío, pío, pío";
+
+        //Mostar info canario
+        System.out.println("Presentando a la " + mascota5);
+        System.out.println("¿Qué es? Un " + tipoCanario);
+        System.out.println("¿Cómo lo llamo? " + nombreCanario);
+        System.out.println("¿Cuántos años tiene? " + edadCanario + " año");
+        System.out.println("¿Cuánto come? " + cuantoComeCanario + " kilo.");
+        System.out.println("¿Onomatopeya de su canto? " + sonidoCanario);
+    }
+}
